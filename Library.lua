4d9d08534d885373fa29d511295cb4124f152d70
Crit-maker <69611506+Crit-maker@users.noreply.github.com>
2022-01-01 02:21:41 -0500

    Library

---
+++ Library.lua
@@ -0,0 +1,7 @@
+local plr = game.Players
+local localplayer = plr.LocalPlayer
+local character = localplayer.Character
+
+local RP = game:GetService("ReplicatedStorage")
+local RS = game:GetService("RunService")
+local TS = game:GetService("TweenService")
